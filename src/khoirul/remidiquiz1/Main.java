829a0ea32e420fb6d6655a2729ec8c1328c98e3f
mohkhoirularifin <karifin998@gmail.com>
2018-11-04 23:17:49 +0700

    Menambahkan Komentar

--- src/khoirul/remidiquiz1/Main.java
+++ src/khoirul/remidiquiz1/Main.java
@@ -1,12 +1,14 @@
 package khoirul.remidiquiz1;
 public class Main {
     public static void main(String[] args) {
+        //mengisi hal yang dibutuhkan
         Mouse nama = new Mouse ("Razer","Razer One");
         Keyboard jenis = new Keyboard ("Razer","USB");
         Cpu processor = new Cpu ("Intel Core i9","9900HQ");
         Komputer komputer = new Komputer ("ASUS AIO","Gaming PC", nama, jenis, processor, 35000000, 0.75f);
         
         System.out.println("\tKhoirul Comp. ");
+        //pemanggilan semua info yang ada pada class komputer
         System.out.println(komputer.info());
     }
     
