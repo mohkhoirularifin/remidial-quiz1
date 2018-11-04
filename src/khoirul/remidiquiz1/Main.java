21de97987086c997c07a16f752419e3de8c4361a
mohkhoirularifin <karifin998@gmail.com>
2018-11-04 20:31:48 +0700

    Edit Class Main

--- src/khoirul/remidiquiz1/Main.java
+++ src/khoirul/remidiquiz1/Main.java
@@ -1,7 +1,13 @@
 package khoirul.remidiquiz1;
 public class Main {
     public static void main(String[] args) {
+        Mouse nama = new Mouse ("Razer","Razer One");
+        Keyboard jenis = new Keyboard ("Razer","USB");
+        Cpu processor = new Cpu ("Intel Core i9");
+        Komputer komputer = new Komputer ("ASUS AIO","Gaming PC", nama, jenis, processor, 35000000);
         
+        System.out.println("\t  Khoirul Comp. ");
+        System.out.println(komputer.info());
     }
     
 }
