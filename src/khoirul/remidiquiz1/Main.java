a8e5f6f55579290d69b5dfb681b84cb7f1f34c90
mohkhoirularifin <karifin998@gmail.com>
2018-11-04 22:09:20 +0700

    Menambahkan besaran diskon

--- src/khoirul/remidiquiz1/Main.java
+++ src/khoirul/remidiquiz1/Main.java
@@ -3,8 +3,8 @@ public class Main {
     public static void main(String[] args) {
         Mouse nama = new Mouse ("Razer","Razer One");
         Keyboard jenis = new Keyboard ("Razer","USB");
-        Cpu processor = new Cpu ("Intel Core i9");
-        Komputer komputer = new Komputer ("ASUS AIO","Gaming PC", nama, jenis, processor, 35000000);
+        Cpu processor = new Cpu ("Intel Core i9","9900HQ");
+        Komputer komputer = new Komputer ("ASUS AIO","Gaming PC", nama, jenis, processor, 35000000, 0.75f);
         
         System.out.println("\tKhoirul Comp. ");
         System.out.println(komputer.info());
