cc0bb769675d1ec6f9783d9bd5469629fb7282c4
mohkhoirularifin <karifin998@gmail.com>
2018-11-04 20:12:30 +0700

    Pembuatan Class Mouse

--- src/khoirul/remidiquiz1/Mouse.java
+++ src/khoirul/remidiquiz1/Mouse.java
@@ -1,4 +1,37 @@
 package khoirul.remidiquiz1;
 public class Mouse {
+    private String nama;
+    private String produk;
     
+    public Mouse(){
+        
+    }
+
+    public Mouse(String nama, String produk) {
+        this.nama = nama;
+        this.produk = produk;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getProduk() {
+        return produk;
+    }
+
+    public void setProduk(String produk) {
+        this.produk = produk;
+    }
+    
+    public String info(){
+        String info = "";
+        info += this.nama + "\n";
+        info += "Produk Mouse\t: " + this.produk;
+        return info;
+    }
 }
